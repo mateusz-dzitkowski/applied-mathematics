ff51709ab4aeeb1492b11480a787de1518eec2af
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2024-05-22 09:13:06 +0200

    I don't remember I did that yesterday

--- projects/diffusion_processes/exercise_sheet_5/exercise_2.py
+++ projects/diffusion_processes/exercise_sheet_5/exercise_2.py
@@ -1,7 +1,11 @@
 from enum import StrEnum, auto
 from random import randint, uniform
+from typing import TypeAlias
 
 import networkx as nx
+import numpy as np
+from nptyping import NDArray
+from plotly import express as xp
 
 
 STATE = "state"
@@ -13,24 +17,16 @@ class NodeState(StrEnum):
     REMOVED = auto()
 
 
+Snapshot: TypeAlias = dict[int, NodeState]
+Simulation: TypeAlias = list[Snapshot]
+
+
 def get_infectious_nodes(graph: nx.Graph) -> list[int]:
     return [node for node, data in graph.nodes(data=True) if data[STATE] == NodeState.INFECTIOUS]
 
 
 def get_susceptible_neighbours_of(node: int, graph: nx.Graph) -> list[int]:
     return [node for node in nx.neighbors(graph, node) if graph.nodes[node][STATE] == NodeState.SUSCEPTIBLE]
-
-
-def step_forward(graph: nx.Graph, p: float) -> nx.Graph:
-    next = graph.copy()
-
-    for infectious in get_infectious_nodes(graph):
-        for neighbour in get_susceptible_neighbours_of(infectious, graph):
-            if uniform(0, 1) < p:
-                next.nodes[neighbour][STATE] = NodeState.INFECTIOUS
-        next.nodes[infectious][STATE] = NodeState.REMOVED
-
-    return next
 
 
 def init_as_susceptible(graph: nx.Graph) -> nx.Graph:
@@ -45,17 +41,60 @@ def spawn_infectious(graph: nx.Graph, node_id: int | None = None) -> nx.Graph:
     return graph
 
 
+def step_forward(graph: nx.Graph, p: float) -> nx.Graph:
+    next = graph.copy()
+
+    for infectious in get_infectious_nodes(graph):
+        for neighbour in get_susceptible_neighbours_of(infectious, graph):
+            if uniform(0, 1) < p:
+                next.nodes[neighbour][STATE] = NodeState.INFECTIOUS
+        next.nodes[infectious][STATE] = NodeState.REMOVED
+
+    return next
+
+
+def take_snapshot(graph: nx.Graph) -> Snapshot:
+    return nx.get_node_attributes(graph, STATE)
+
+
+def simulate(graph: nx.Graph, p: float, max_steps: int, initial_infectious: int | None = None, shortcircuit: bool = False) -> Simulation:
+    graph = graph.copy()
+    graph = init_as_susceptible(graph)
+    graph = spawn_infectious(graph, initial_infectious)
+
+    simulation: Simulation = [take_snapshot(graph)]
+
+    for _ in range(max_steps - 1):
+        graph = step_forward(graph, p)
+        simulation.append(take_snapshot(graph))
+
+        if shortcircuit and not get_infectious_nodes(graph):
+            break
+
+    return simulation
+
+
+def get_fraction_of_infected_nodes(simulation: Simulation) -> list[float]:
+    return [
+        len([1 for state in step.values() if state == NodeState.INFECTIOUS]) / len(step) for step in simulation
+    ]
+
+
+def get_average_fraction_of_infected_nodes(graph: nx.Graph, p: float, max_steps: int, num_runs: int, initial_infectious: int) -> NDArray:
+    simulations = [simulate(graph=graph, p=p, max_steps=max_steps, initial_infectious=initial_infectious) for _ in range(num_runs)]
+    fractions = np.array([get_fraction_of_infected_nodes(simulation) for simulation in simulations])
+    return fractions.mean(axis=0)
+
+
 def main():
-    g = nx.erdos_renyi_graph(3, 0.8)
-    g = init_as_susceptible(g)
-    g = spawn_infectious(g)
-
-    h = step_forward(g, 0.5)
-    i = step_forward(h, 0.5)
-
-    print(nx.get_node_attributes(g, STATE))
-    print(nx.get_node_attributes(h, STATE))
-    print(nx.get_node_attributes(i, STATE))
+    averages = get_average_fraction_of_infected_nodes(
+        graph=nx.erdos_renyi_graph(100, 0.4),
+        p=0.1,
+        max_steps=10,
+        num_runs=100,
+        initial_infectious=0,
+    )
+    xp.line(y=averages).show()
 
 
 if __name__ == "__main__":
