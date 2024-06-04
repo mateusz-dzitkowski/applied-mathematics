6b4b54066360444453a7a88ee85100cd068b747b
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2024-06-04 17:29:13 +0200

    add animations and other stuff to diffusion processes also poetry lock

--- projects/diffusion_processes/exercise_sheet_5/exercise_2.py
+++ projects/diffusion_processes/exercise_sheet_5/exercise_2.py
@@ -1,16 +1,22 @@
 from dataclasses import dataclass, asdict
 from enum import StrEnum, auto
 from random import choice, uniform
-from typing import TypeAlias
+from typing import TypeAlias, Protocol
 
 import networkx as nx
 import numpy as np
 from nptyping import NDArray
 from plotly import express as xp
 from pandas import DataFrame
+from matplotlib import (
+    animation,
+    pyplot as plt,
+)
 
 
 STATE = "state"
+FPS = 1
+BITRATE = 2400
 
 
 @dataclass
@@ -27,6 +33,10 @@ class NodeState(StrEnum):
 
 
 Step: TypeAlias = dict[int, NodeState]
+
+
+class StepForward(Protocol):
+    def __call__(self, graph: nx.Graph, p: float, q: float = ..., /) -> nx.Graph: ...
 
 
 @dataclass
@@ -64,8 +74,8 @@ class Simulation:
         )
 
 
-def get_infectious_nodes(graph: nx.Graph) -> list[int]:
-    return [node for node, data in graph.nodes(data=True) if data[STATE] == NodeState.INFECTIOUS]
+def get_nodes(graph: nx.Graph, state: NodeState) -> list[int]:
+    return [node for node, data in graph.nodes(data=True) if data[STATE] == state]
 
 
 def get_susceptible_neighbours_of(node: int, graph: nx.Graph) -> list[int]:
@@ -77,21 +87,42 @@ def init_as_susceptible(graph: nx.Graph) -> nx.Graph:
     return graph
 
 
-def spawn_infectious(graph: nx.Graph, node_id: int | None = None) -> nx.Graph:
+def spawn_infectious(graph: nx.Graph, node_id: int | tuple[int, int] | None = None) -> nx.Graph:
     if node_id is None:
         node_id = choice(list(graph.nodes))
     graph.nodes[node_id][STATE] = NodeState.INFECTIOUS
     return graph
 
 
-def step_forward(graph: nx.Graph, p: float) -> nx.Graph:
+def step_forward_asynchronous(graph: nx.Graph, p: float, q: float = 0.0) -> nx.Graph:
+    for _ in range(len(graph.nodes)):
+        node = choice(list(graph.nodes))
+        state = graph.nodes[node][STATE]
+
+        if state == NodeState.INFECTIOUS:
+            for neighbour in get_susceptible_neighbours_of(node, graph):
+                if uniform(0, 1) < p:
+                    graph.nodes[neighbour][STATE] = NodeState.INFECTIOUS
+            graph.nodes[node][STATE] = NodeState.REMOVED
+        elif state == NodeState.REMOVED:
+            if uniform(0, 1) < q:
+                graph.nodes[node][STATE] = NodeState.SUSCEPTIBLE
+
+    return graph
+
+
+def step_forward_synchronous(graph: nx.Graph, p: float, q: float = 0.0) -> nx.Graph:
     next = graph.copy()
 
-    for infectious in get_infectious_nodes(graph):
+    for infectious in get_nodes(graph, NodeState.INFECTIOUS):
         for neighbour in get_susceptible_neighbours_of(infectious, graph):
             if uniform(0, 1) < p:
                 next.nodes[neighbour][STATE] = NodeState.INFECTIOUS
         next.nodes[infectious][STATE] = NodeState.REMOVED
+
+    for removed in get_nodes(graph, NodeState.REMOVED):
+        if uniform(0, 1) < q:
+            next.nodes[removed][STATE] = NodeState.SUSCEPTIBLE
 
     return next
 
@@ -100,7 +131,15 @@ def take_snapshot(graph: nx.Graph) -> Step:
     return nx.get_node_attributes(graph, STATE)
 
 
-def simulate(graph: nx.Graph, p: float, max_steps: int, initial_infectious: int | None = None, shortcircuit: bool = False) -> Simulation:
+def simulate(
+    graph: nx.Graph,
+    max_steps: int,
+    p: float,
+    q: float = 0.0,
+    initial_infectious: int | tuple[int, int] | None = None,
+    shortcircuit: bool = False,
+    step_forward: StepForward = step_forward_synchronous,
+) -> Simulation:
     graph = graph.copy()
     graph = init_as_susceptible(graph)
     graph = spawn_infectious(graph, initial_infectious)
@@ -108,10 +147,10 @@ def simulate(graph: nx.Graph, p: float, max_steps: int, initial_infectious: int 
     simulation: Simulation = Simulation([take_snapshot(graph)])
 
     for _ in range(max_steps - 1):
-        graph = step_forward(graph, p)
+        graph = step_forward(graph, p, q)
         simulation.add_step(take_snapshot(graph))
 
-        if shortcircuit and not get_infectious_nodes(graph):
+        if shortcircuit and not get_nodes(graph, NodeState.INFECTIOUS):
             break
 
     return simulation
@@ -125,7 +164,7 @@ def get_average_fraction_of_infected_nodes(graph: nx.Graph, p: float, max_steps:
 
 def get_average_measures(graph: nx.Graph, p: float, max_steps: int, num_runs: int = 300) -> Measures:
     print(f"getting measures for {p=}")
-    list_measures = [simulate(graph, p, max_steps, shortcircuit=True).to_measures() for _ in range(num_runs)]
+    list_measures = [simulate(graph, max_steps, p, shortcircuit=True).to_measures() for _ in range(num_runs)]
     return Measures(
         total_infected_proportion=np.mean([measure.total_infected_proportion for measure in list_measures]),
         time_to_clear=np.mean([measure.time_to_clear for measure in list_measures]),
@@ -142,11 +181,10 @@ def get_measures_per_p(graph: nx.Graph, max_steps: int, num_p: int = 20) -> Data
 
 
 def show_measures():
-    # TODO: stopien wierzcholka taki sam
     twod_lattice = get_measures_per_p(nx.grid_2d_graph(10, 10), 100)
-    random = get_measures_per_p(nx.erdos_renyi_graph(100, 0.4), 100)
+    random = get_measures_per_p(nx.erdos_renyi_graph(100, 0.0365), 100)
     watts_strogatz = get_measures_per_p(nx.watts_strogatz_graph(100, 4, 0.4), 100)
-    barabasi_albert = get_measures_per_p(nx.barabasi_albert_graph(100, 4), 100)
+    barabasi_albert = get_measures_per_p(nx.barabasi_albert_graph(100, 2), 100)
 
     total_infected = DataFrame(data={
         "p": twod_lattice["p"],
@@ -186,6 +224,37 @@ def show_measures():
         x="p",
         y=["twod_lattice", "random", "watts_strogatz", "barabasi_albert"],
     ).show()
+
+
+def animate(g: nx.Graph, simulation: Simulation, filename: str, grid_layout: bool = False):
+    # planar layout: used for 2d grid graph
+    layout = {(x, y): (y, -x) for x, y in g.nodes()} if grid_layout else nx.circular_layout(g)
+
+    fig, ax = plt.subplots()
+
+    def animate(n: int):
+        step = simulation.steps[n]
+
+        susceptible = [index for index, state in step.items() if state == NodeState.SUSCEPTIBLE]
+        infectious = [index for index, state in step.items() if state == NodeState.INFECTIOUS]
+        removed = [index for index, state in step.items() if state == NodeState.REMOVED]
+
+        ax.clear()
+        nx.draw_networkx_nodes(g, layout, nodelist=susceptible, node_color="green")
+        nx.draw_networkx_nodes(g, layout, nodelist=infectious, node_color="red")
+        nx.draw_networkx_nodes(g, layout, nodelist=removed, node_color="black")
+        nx.draw_networkx_edges(g, layout)
+
+    print("saving the animation...")
+    animation.FuncAnimation(
+        fig=fig,
+        func=animate,  # type: ignore
+        frames=len(simulation.steps),
+    ).save(
+        filename=filename,
+        writer=animation.PillowWriter(fps=FPS, bitrate=BITRATE),
+    )
+    plt.close()
 
 
 def main():
