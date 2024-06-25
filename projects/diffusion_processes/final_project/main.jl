aa58da9f1f97c6e8b1aab12b1d87ce0a27af1cbd
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2024-06-25 12:00:04 +0200

    improve remove that shit bitch module shit I hate this make the api more convenient

--- projects/diffusion_processes/final_project/main.jl
+++ projects/diffusion_processes/final_project/main.jl
@@ -1,35 +1,134 @@
 using Graphs
 using StatsBase
 
-include("ThresholdModel.jl")
-using .ThresholdModel
+struct Params
+    threshold :: AbstractFloat
+    profile :: AbstractFloat
+    immunization_probability :: AbstractFloat
+    spontaneous_adoption :: AbstractFloat
+end
 
-include("ProfileModel.jl")
-using .ProfileModel
+struct State
+    graph :: Graphs.SimpleGraph
+    infected :: AbstractSet
+    blocked :: AbstractSet
+    params :: Params
+end
 
-include("ProfileThresholdModel.jl")
-using .ProfileThresholdModel
+function get_initial_state(graph :: Graphs.SimpleGraph, params :: Params, percent_initially_infected :: AbstractFloat)
+    graph_length = Graphs.nv(graph)
+    initially_infected = trunc(Int, graph_length * percent_initially_infected)
+    State(
+        graph,
+        Set(StatsBase.sample(1:graph_length, initially_infected, replace=false)),
+        Set(),
+        params,
+    )
+end
 
+function run(state :: State, step_function :: Function, num_steps :: Integer) Vector{Integer}
+    infected_count = Vector{Integer}()
+    push!(infected_count, length(state.infected))
+    for _ = 1:num_steps
+        state = step_function(state)
+        push!(infected_count, length(state.infected))
+    end
+    infected_count
+end
+
+function profile_model(state::State) State
+    new_infected = copy(state.infected)
+    for node in Graphs.vertices(state.graph)
+        if rand() < state.params.spontaneous_adoption
+            push!(new_infected, node)
+            continue
+        end
+
+        if in(node, state.infected) || in(node, state.blocked)
+            continue
+        end
+
+        neighbors = Set(Graphs.neighbors(state.graph, node))
+        infected_neighbors = intersect(state.infected, neighbors)
+        if length(infected_neighbors) == 0
+            continue
+        end
+
+        if rand() >= state.params.profile
+            push!(new_infected, node)
+        elseif rand() <= state.params.immunization_probability
+            push!(state.blocked, node)
+        end
+
+    end
+
+    State(state.graph, new_infected, state.blocked, state.params)
+end
+
+function profile_threshold_model(state::State) State
+    new_infected = copy(state.infected)
+    for node in Graphs.vertices(state.graph)
+        if rand() < state.params.spontaneous_adoption
+            push!(new_infected, node)
+            continue
+        end
+
+        if in(node, state.infected) || in(node, state.blocked)
+            continue
+        end
+
+        neighbors = Set(Graphs.neighbors(state.graph, node))
+        infected_neighbors = length(intersect(state.infected, neighbors))
+        if infected_neighbors == 0
+            continue
+        end
+
+        if infected_neighbors / length(neighbors) >= state.params.threshold
+            if rand() >= state.params.profile
+                push!(new_infected, node)
+            end
+        elseif rand() <= state.params.immunization_probability
+            push!(state.blocked, node)
+        end
+
+    end
+
+    State(state.graph, new_infected, state.blocked, state.params)
+end
+
+function threshold_model(state::State) State
+    new_infected = copy(state.infected)
+    for node in Graphs.vertices(state.graph)
+        if rand() < state.params.spontaneous_adoption
+            push!(new_infected, node)
+            continue
+        end
+
+        if in(node, state.infected)
+            continue
+        end
+
+        neighbors = Set(Graphs.neighbors(state.graph, node))
+        infected_neighbors = length(intersect(state.infected, neighbors))
+
+        if infected_neighbors / length(neighbors) >= state.params.threshold
+            push!(new_infected, node)
+        end
+    end
+
+    State(state.graph, new_infected, state.blocked ,state.params)
+end
 
 function main()
-    n = 63392
-    m = 13
-    k = 3150
-    tau = 0.1
+    params = Params(0.1, 0.2, 0, 0)
 
-
-    graph = barabasi_albert(n, m)
-
-    infected_counts = ProfileModel.run(
-        ProfileModel.State(
-            graph,
-            Set(StatsBase.sample(1:n, k, replace=false)),
-            Set(),
-            0.8,
-            0,
-        ),
-        30,
+    initial_state = get_initial_state(
+        barabasi_albert(63392, 13),
+        params,
+        0.05,
     )
+
+    infected_counts = run(initial_state, profile_model, 30)
     println(infected_counts)
 end
 
