40600886f5b49afc35bc95160271e8c1f10c0b8a
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2024-06-29 09:56:52 +0200

    plottinf the average of n runs

--- projects/diffusion_processes/final_project/main.jl
+++ projects/diffusion_processes/final_project/main.jl
@@ -1,4 +1,5 @@
 using Graphs
+using Plots
 using StatsBase
 
 struct Params
@@ -27,13 +28,28 @@ function get_initial_state(graph :: Graphs.SimpleGraph, params :: Params, percen
 end
 
 function run(state :: State, step_function :: Function, num_steps :: Integer) Vector{Integer}
-    infected_count = Vector{Integer}()
-    push!(infected_count, length(state.infected))
-    for _ = 1:num_steps
+    infected_count = zeros(Int64, num_steps + 1)
+    infected_count[1] = length(state.infected)
+    for n = 2:num_steps + 1
         state = step_function(state)
-        push!(infected_count, length(state.infected))
+        infected_count[n] = length(state.infected)
     end
     infected_count
+end
+
+function run_average(
+    graph :: Graphs.SimpleGraph,
+    step_function :: Function,
+    params :: Params,
+    num_steps :: Integer = 30,
+    num_runs :: Integer = 20,
+)
+    output = zeros(Int64, num_steps + 1)
+    for n = 1:num_runs
+        initial_state = get_initial_state(graph, params, 0.05)
+        output += run(initial_state, step_function, num_steps)
+    end
+    return output / num_runs
 end
 
 function profile_model(state::State) State
@@ -120,16 +136,23 @@ function threshold_model(state::State) State
 end
 
 function main()
-    params = Params(0.1, 0.2, 0, 0)
+    params = Params(0.2, 0.4, 0, 0)
+    graph = Graphs.barabasi_albert(63392, 13)
 
-    initial_state = get_initial_state(
-        barabasi_albert(63392, 13),
-        params,
-        0.05,
+    p = run_average(graph, profile_model, params)
+    t = run_average(graph, threshold_model, params)
+    pt = run_average(graph, profile_threshold_model, params)
+
+    Plots.savefig(
+        Plots.plot(
+            [p t pt],
+            labels=["profile" "threshold" "profile-threshold"],
+            xlabel="Iterations",
+            ylabel="Infected nodes",
+            legend=:bottomright,
+        ),
+        "ba_a.pdf",
     )
-
-    infected_counts = run(initial_state, profile_model, 30)
-    println(infected_counts)
 end
 
 main()
