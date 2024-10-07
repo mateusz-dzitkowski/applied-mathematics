b4a35fdefc1ae8018b1b15b4ddf0c163e44ee0ff
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2024-10-07 22:31:11 +0200

    plot

--- projects/agent_based_modelling/assignment_1/batch.py
+++ projects/agent_based_modelling/assignment_1/batch.py
@@ -1,19 +1,23 @@
-import os
+from matplotlib import pyplot as plt
 from mesa.batchrunner import batch_run
 import numpy as np
+from pandas import DataFrame
 
-from projects.agent_based_modelling.assignment_1.forest_fire import Tree, ForestFire, TreeState
+from projects.agent_based_modelling.assignment_1.forest_fire import ForestFire, TreeState
 
 
 if __name__ == "__main__":
-    for item in batch_run(
+    results = batch_run(
         model_cls=ForestFire,
         parameters={
             "width": 100,
             "height": 100,
-            "p": np.arange(start=0, stop=1, step=0.1),
+            "p": np.arange(start=0, stop=1, step=0.01),
         },
-        iterations=2,
+        iterations=10,
         number_processes=None,
-    ):
-        print(item)
+    )
+
+    df = DataFrame(data=results)
+    df[["p", TreeState.FINE, TreeState.BURNED_DOWN]].groupby("p").mean().plot()
+    plt.show()
