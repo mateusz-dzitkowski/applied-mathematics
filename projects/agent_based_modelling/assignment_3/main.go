92501dbade26b8d20897c2c2f9abd0bc4100f67c
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2024-10-30 08:47:14 +0100

    redo the list 3 thing

--- projects/agent_based_modelling/assignment_3/main.go
+++ projects/agent_based_modelling/assignment_3/main.go
@@ -2,31 +2,10 @@ package main
 
 import (
 	"fmt"
-	"main/tree"
+	"main/model"
 )
-
-const (
-	BLUE = "blue"
-	RED  = "red"
-)
-
-type Agent struct {
-	color string
-}
 
 func main() {
-	t := tree.New[Agent]()
-	agents := []tree.Object[Agent]{
-		{Pos: tree.Position{X: 3, Y: 3}, Props: Agent{color: RED}},
-		{Pos: tree.Position{X: 2, Y: 4}, Props: Agent{color: BLUE}},
-		{Pos: tree.Position{X: 3, Y: 5}, Props: Agent{color: RED}},
-		{Pos: tree.Position{X: 1, Y: 2}, Props: Agent{color: BLUE}},
-		{Pos: tree.Position{X: 3, Y: 1}, Props: Agent{color: RED}},
-		{Pos: tree.Position{X: 4, Y: 3}, Props: Agent{color: RED}},
-		{Pos: tree.Position{X: 5, Y: 1}, Props: Agent{color: RED}},
-	}
-	for _, agent := range agents {
-		t.Insert(agent)
-	}
-	fmt.Println(t.FindKNearestNeighbours(tree.Position{X: 3, Y: 4}, 9))
+	m := model.New(model.Params{Size: 100, Blues: 250, Reds: 250})
+	fmt.Println(m.Tree.Points())
 }
