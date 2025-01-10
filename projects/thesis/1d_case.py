2b52c8d70f86a8b348670aa3b8775517242c610b
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2025-01-10 21:33:28 +0100

    works nice without the need to mess with f

--- projects/thesis/1d_case.py
+++ projects/thesis/1d_case.py
@@ -33,42 +33,48 @@ def my_smoothing(beta: float) -> Func:
     return inner
 
 
-def solve_once(*, x: np.ndarray, c_full: np.ndarray, f_full: np.ndarray, alpha: float = 1.0) -> np.ndarray:
+def solve_once(*, x: np.ndarray, c: np.ndarray, f: np.ndarray, alpha: float = 1.0) -> np.ndarray:
     h = np.diff(x)[0]  # assume that discretization is uniform YOLO
     n = x.shape[0]
 
-    c_plus = c_full[2:]
-    c_minus = c_full[:-2]
-    c = c_full[1:-1]
-    c_plus_half = (c_plus + c) / 2
-    c_minus_half = (c_minus + c) / 2
+    c_plus = c[2:]
+    c_minus = c[:-2]
+    c_mid = c[1:-1]
+    c_plus_half = (c_plus + c_mid) / 2
+    c_minus_half = (c_minus + c_mid) / 2
 
-    f = f_full
-    f[0] = f[-1] = 0
+    almost_diagonal = 1 + alpha / h**2 * (c_plus_half + c_minus_half)
+    almost_superdiagonal = -alpha / h**2 * c_plus_half
+    almost_subdiagonal = -alpha / h**2 * c_minus_half
 
-    diagonal = 1 + alpha / h**2 * (c_plus_half + c_minus_half)
-    superdiagonal = -alpha / h**2 * c_plus_half
-    subdiagonal = -alpha / h**2 * c_minus_half
+    diagonal = np.hstack(
+        [
+            np.array([1 + alpha/h**2*(c[0]+c[1])/2]),
+            almost_diagonal,
+            np.array([1 + alpha/h**2*(c[-1]+c[-2])/2])
+        ],
+    )
+    superdiagonal = np.hstack([np.array([-alpha/h**2*(c[0]+c[1])/2]), almost_superdiagonal])
+    subdiagonal = np.hstack([almost_subdiagonal, np.array([-alpha/h**2*(c[-1]+c[-2])/2])])
 
     a = np.zeros((n, n))
-    np.fill_diagonal(a, np.hstack([np.array([1]), diagonal, np.array([1])]))
-    a[kth_diag_indices(a, 1)] = np.hstack([np.array([-1]), superdiagonal])
-    a[kth_diag_indices(a, -1)] = np.hstack([subdiagonal, np.array([-1])])
+    np.fill_diagonal(a, diagonal)
+    a[kth_diag_indices(a, 1)] = superdiagonal
+    a[kth_diag_indices(a, -1)] = subdiagonal
 
     return np.linalg.solve(a, f)
 
 
-def solve(*, x: np.ndarray, func_f: Func, smoothing_f: Func, alpha: float = 1.0, steps: int = 100) -> np.ndarray:
-    f_full = func_f(x)
-    u = np.copy(f_full)
+def solve(*, x: np.ndarray, f: np.ndarray, smoothing_f: Func, alpha: float = 1.0, steps: int = 100) -> np.ndarray:
+    u = np.copy(f)
 
     for _ in range(steps):
         u_diff = np.diff(u, prepend=u[0])
-        c_full = smoothing_f(u_diff)
+        c = smoothing_f(u_diff)
         u = solve_once(
             x=x,
-            c_full=c_full,
-            f_full=f_full,
+            c=c,
+            f=f,
             alpha=alpha,
         )
 
@@ -77,26 +83,33 @@ def solve(*, x: np.ndarray, func_f: Func, smoothing_f: Func, alpha: float = 1.0,
 
 def main():
     x = np.linspace(0, 1, 1000, endpoint=True)
-    alpha = 0.000001
-    beta = 0.001
+    alpha = 0.0000001
+    beta = 0.00001
 
-    randomness = np.random.normal(scale=0.03, size=x.size)
-    func_f = lambda _x: np.heaviside(_x - 0.4, 1) - np.heaviside(_x - 0.6, 1) + randomness
+    def func_f(_x: np.ndarray) -> np.ndarray:
+        return 1 + np.heaviside(_x - 0.4, 1) - np.heaviside(_x - 0.7, 1) + np.cos(np.pi*_x)
 
-    plt.figure(figsize=(10, 8), dpi=300)
+    f_with_randomness = func_f(x) + np.random.normal(scale=0.03, size=x.size)
+
+    plt.figure(figsize=(15, 11), dpi=300)
     plt.plot(
         x,
         solve(
             x=x,
             smoothing_f=vo_smoothing(beta),
-            func_f=func_f,
+            f=f_with_randomness,
             alpha=alpha,
         ),
-        label="with no noise",
+        label="denoised",
     )
     plt.plot(
         x,
         func_f(x),
+        label="with no noise",
+    )
+    plt.plot(
+        x,
+        f_with_randomness,
         "r.",
         markersize=1,
         label="with noise",
